7923a6dd2c0727705f2333c50e9e94af58dec078
Meval Purohit <mevalpurohit2828@gmail.com>
2026-03-28 17:49:00 +0530

    added the run build

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from "path"
+import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.routes.js';
 import brainRoutes from './routes/brain.routes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors({
@@ -13,9 +18,13 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 
 app.use('/api/auth', authRoutes);
 app.use('/api/brain', brainRoutes);
+app.use("*name",(req,res)=>{
+res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+})
 
 export default app;
