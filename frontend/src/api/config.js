7545f7ba5e187334379d798db943d20080f0dd7e
Meval Purohit <mevalpurohit2828@gmail.com>
2026-04-02 14:23:40 +0530

    added tthe run build again for the config.api

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios" 
 
 const api = axios.create({
-    baseURL:import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+    baseURL:"https://braniac-66vs.onrender.com/api",
     withCredentials:true
 })
 
